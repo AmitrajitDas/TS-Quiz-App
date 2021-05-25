c3d37f8d46d9129e0bba08ea5612e492ed837547
Amitrajit Das <amitrajitdas31@gmail.com>
2021-05-25 14:12:17 +0530

    startTrivia, answer check , next question methods are added & structured question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -18,8 +18,8 @@ const QuestionCard: React.FC<Props> = ({ question,answers,callback,userAnswer,qu
             <p dangerouslySetInnerHTML={{__html: question}} />
             <div>
                 {answers.map((answer => (
-                    <div>
-                        <button disabled={userAnswer} onClick={callback}>
+                    <div key={answer}>
+                        <button disabled={userAnswer} value={answer} onClick={callback}>
                             <span dangerouslySetInnerHTML={{__html: answer}} />
                         </button>
                     </div>
